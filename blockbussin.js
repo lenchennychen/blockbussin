5c7cd2967cec4269572f5332495cb488c9e2a3a3
Seongbin Park <shannonsbpark@gmail.com>
2022-02-22 16:28:00 -0800

    made blocks

--- blockbussin.js
+++ blockbussin.js
@@ -24,6 +24,12 @@ export class blockbussin extends Scene {
     constructor() {
         super();
 
+        this.transformations = [['R', 'R', 'R'],
+                                ['D', 'R', 'D'],
+                                ['R', 'D', 'L'],
+                                ['R', 'R', 'T'],
+                                ['D', 'D', 'R']];
+
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
             torus: new defs.Torus(15, 15),
@@ -46,6 +52,8 @@ export class blockbussin extends Scene {
         }
         this.white = new Material(new defs.Basic_Shader());
 
+        this.current_block = null;
+
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
     }
 
@@ -59,14 +67,14 @@ export class blockbussin extends Scene {
         this.key_triggered_button("Attach to planet 3", ["Control", "3"], () => this.attached = () => this.planet_3);
         this.key_triggered_button("Attach to planet 4", ["Control", "4"], () => this.attached = () => this.planet_4);
         this.new_line();
-        this.key_triggered_button("Attach to moon", ["Control", "m"], () => this.attached = () => this.moon);
+        this.key_triggered_button("Drop Block", [" "], () => this.current_block = null);
     }
 
     display(context, program_state) {
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            //this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(this.initial_camera_location);
         }
@@ -78,24 +86,48 @@ export class blockbussin extends Scene {
         // The parameters of the Light are: position, color, size
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
+        // TODO: only call draw Block
+
+        // only set new this.current_block if new block is needed
+        if(this.current_block == null) {
+            this.current_block = Math.floor(Math.random() * 5);
+        }
+        this.drawBlock(context, program_state, this.current_block);
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+
+    }
+
+    drawBlock(context, program_state, n) {
+        const cur_trans = this.transformations[n];
+
+        // draw initial block
+        // TODO: change starting point
         let model_transform = Mat4.identity();
         this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic);
         this.shapes.outline.draw(context, program_state, model_transform, this.white,"LINES");
-        model_transform = model_transform.times(Mat4.translation(0,2,0))
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic);
 
-        this.shapes.outline.draw(context, program_state, model_transform, this.white,"LINES");
-        model_transform = model_transform.times(Mat4.translation(0,2,0))
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic);
-
-        this.shapes.outline.draw(context, program_state, model_transform, this.white,"LINES");
-        model_transform = model_transform.times(Mat4.translation(2,0,0))
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic);
-
-        this.shapes.outline.draw(context, program_state, model_transform, this.white,"LINES");
-
-
+        for (const element of cur_trans) {
+            switch (element) {
+                case 'R':
+                    model_transform = model_transform.times(Mat4.translation(2,0,0));
+                    break;
+                case 'D': 
+                    model_transform = model_transform.times(Mat4.translation(0,-2,0));
+                    break;
+                case 'L':
+                    model_transform = model_transform.times(Mat4.translation(-2,0,0));
+                    break;
+                case 'U':
+                    model_transform = model_transform.times(Mat4.translation(0,2,0));
+                    break;
+                case 'T': 
+                    model_transform = model_transform.times(Mat4.translation(-2,-2,0));
+                default:
+                    break;
+            }
+            this.shapes.outline.draw(context, program_state, model_transform, this.white,"LINES");
+            this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic);
+        }
     }
 }
 
